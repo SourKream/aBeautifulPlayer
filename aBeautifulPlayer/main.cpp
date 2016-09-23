96c3b380ba5c9dc4526792cff09fd3d409899cb4
Shantanu Kumar <kumarshantanu95@gmail.com>
2016-09-23 23:47:29 +0530

    Initial Commit

---
+++ aBeautifulPlayer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  aBeautifulPlayer
+//
+//  Created by Shantanu Kumar on 23/09/16.
+//  Copyright © 2016 Shantanu Kumar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
