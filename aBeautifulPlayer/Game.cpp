38fdc474cf8667b42ee7b2c840815c8508d85f18
Akshay Singla <akshay.singla27@gmail.com>
2016-10-01 14:45:03 +0530

    Apply Moves added

--- aBeautifulPlayer/Game.cpp
+++ aBeautifulPlayer/Game.cpp
@@ -7,10 +7,14 @@
 //
 
 #include "Game.hpp"
-
+#include <string.h>
+#include <iostream>
 #define BLACK 0
 #define WHITE 1
 #define MAX_SIZE 7
+#define MAX_SIZE_SQUARE MAX_SIZE*MAX_SIZE
+using namespace std;
+typedef unsigned long long uint64;
 
 enum MoveType { PlaceFlat = 0,
                 PlaceStanding,
@@ -21,55 +25,337 @@ enum MoveType { PlaceFlat = 0,
                 SlideDown
             };
 
+enum PieceType {    FlatStone = 0,
+                    StandingStone,
+                    Capstone
+};
+
+enum Color {    Black = 0,
+    White
+};
+
+class Piece{
+    public :
+    PieceType type;
+    Color color;
+};
+
+
 class Move{
-    MoveType MoveType;
+public :
+    MoveType Movetype;
+    Piece piece;
     short int row;
     short int column;
     short int Drops[MAX_SIZE] = {};
     short int dropLength;
 };
 
+
 class Config{
+public :
     int BoardSize;
     int Pieces;
     int Capstones;
     
-    unsigned long long Left;
-    unsigned long long Right;
-    unsigned long long Top;
-    unsigned long long Bottom;
-    unsigned long long Edge;
-    unsigned long long BoardMask;
+    uint64 Left;
+    uint64 Right;
+    uint64 Top;
+    uint64 Bottom;
+    uint64 Edge;
+    uint64 BoardMask;
     
-    Config(int BoardSize){
+    Config(int boardSize){
         
+        BoardSize = boardSize;
+        switch(BoardSize){
+            case 5:
+                Pieces = 21;
+                Capstones = 1;
+                break;
+            case 6:
+                Pieces = 30;
+                Capstones = 1;
+                break;
+            case 7:
+                Pieces = 40;
+                Capstones = 1;
+                break;
+        }
+        Left = 0;
+        for (int i = 0 ; i < BoardSize*BoardSize ; i += BoardSize)
+            Left |= (1ULL << i);
+        
+        Right = 0;
+        for (int i = BoardSize - 1 ; i < BoardSize*BoardSize ; i += BoardSize)
+            Right |= (1ULL << i);
+        
+        Top = 0;
+        for (int i = 0 ; i < BoardSize ; i++)
+            Top |= (1ULL << i);
+        
+        Bottom = 0;
+        for (int i = BoardSize*BoardSize - BoardSize ; i < BoardSize*BoardSize ; i++)
+            Bottom |= (1ULL << i);
+        
+        Edge = Top | Left | Bottom | Right;
+        
+        
+        BoardMask = (1ULL << (BoardSize*BoardSize)) - 1;
+
     }
 };
 
 class Game{
-    unsigned long long WhitePieces;
-    unsigned long long BlackPieces;
-    unsigned long long CapStones;
-    unsigned long long Standing;
-    unsigned long long Stacks[MAX_SIZE*MAX_SIZE] = {};
-    unsigned long long Heights[MAX_SIZE*MAX_SIZE] = {};
-    
+public :
+    uint64 WhitePieces;
+    uint64 BlackPieces;
+    uint64 CapStones;
+    uint64 Standing;
+    uint64 Stacks[MAX_SIZE_SQUARE];
+    uint64 Heights[MAX_SIZE_SQUARE];
+    uint64 WhiteComponents[MAX_SIZE_SQUARE];
+    uint64 BlackComponents[MAX_SIZE_SQUARE];
+    short int size_cw;
+    short int size_cb;
+    Config *gameConfig;
     unsigned long moves;
     bool currentPlayer = 0;
     
-    Game(){
+    Game(int BoardSize){
         WhitePieces = 0;
         BlackPieces = 0;
         CapStones = 0;
         Standing = 0;
+        gameConfig = new Config(BoardSize);
+        memset(Stacks, 0 , MAX_SIZE_SQUARE*sizeof(uint64));
+        memset(Heights, 0 , MAX_SIZE_SQUARE*sizeof(uint64));
     }
     
-    void applyMove(Move move){
+    Game(Game &current){
+        WhitePieces = current.WhitePieces;
+        BlackPieces = current.BlackPieces;
+        CapStones = current.CapStones;
+        Standing = current.Standing;
+        memcpy(Stacks, current.Stacks, MAX_SIZE_SQUARE*sizeof(uint64));
+        memcpy(Heights, current.Heights, MAX_SIZE_SQUARE*sizeof(uint64));
+        gameConfig = current.gameConfig;
+        currentPlayer = current.currentPlayer;
+        moves = current.moves;
+    }
+    
+    void PlaceMove(Move move){
+        int i = move.row*gameConfig->BoardSize + move.column;
+        Heights[i] += 1;
+        Stacks[i] <<=1;
+        switch (move.piece.color){
+            case Black :
+                BlackPieces |= (1ULL << i);
+                break;
+            case White :
+                WhitePieces |= (1ULL << i);
+                Stacks[i] |= 1;
+                break;
+        }
+        switch (move.piece.type) {
+            case Capstone :
+                CapStones |= (1ULL << i);
+                break;
+            case StandingStone :
+                Standing |= (1ULL << i);
+                break;
+        }
+    }
+    
+    uint64 Expand(uint64 current, uint64 mask){
+        uint64 next = current;
+        do {
+            current = next;
+            next |= (current << 1);
+            next |= (current >> 1);
+            next |= (current >> gameConfig->BoardSize);
+            next |= (current << gameConfig->BoardSize);
+            next &= mask;
+        } while (next !=current);
+        return next;
+    }
+    
+    void FindComponents(){
+        size_cw = 0;
+        uint64 whitePieces = WhitePieces;
+        uint64 seen = 0;
+        uint64 current;
+        uint64 bit;
         
+        while ( whitePieces){
+            current = whitePieces & (whitePieces-1);
+            bit = whitePieces & ~current;
+            if ( (bit & seen) == 0){
+                uint64 comp = Expand(bit,WhitePieces);
+                if ( comp != bit){
+                    WhiteComponents[size_cw++] = comp;
+                }
+                seen |= comp;
+            }
+            whitePieces = current;
+        }
+        
+        size_cb = 0;
+        uint64 blackPieces = BlackPieces;
+        seen = 0;
+        
+        while ( blackPieces){
+            current = blackPieces & (blackPieces-1);
+            bit = blackPieces & ~current;
+            if ( (bit & seen) == 0){
+                uint64 comp = Expand(bit,BlackPieces);
+                if ( comp != bit){
+                    BlackComponents[size_cb++] = comp;
+                }
+                seen |= comp;
+            }
+            blackPieces = current;
+        }
+        return;
     }
     
     
+    int checkIfRoadExists(){
+        // -1 : False
+        // 0 : Black
+        // 1 : White
+        // 2 : Both
+        bool white = false;
+        for (int i =0 ; i <size_cw; i++){
+            if ( ((WhiteComponents[i] & gameConfig->Left) & (WhiteComponents[i] & gameConfig->Right)) != 0 || ((WhiteComponents[i] & gameConfig->Top) & (WhiteComponents[i] & gameConfig->Bottom)) != 0){
+                white = true;
+                break;
+            }
+        }
+        
+                bool black = false;
+        for (int i =0 ; i <size_cb; i++){
+            if ( ((BlackComponents[i] & gameConfig->Left) & (BlackComponents[i] & gameConfig->Right)) != 0 || ((BlackComponents[i] & gameConfig->Top) & (BlackComponents[i] & gameConfig->Bottom)) != 0){
+                black = true;
+                break;
+            }
+        }
+        if ( black && white)
+            return 2;
+        else if (black)
+            return 0;
+        else if (white)
+            return 1;
+        else
+            return -1;
+    }
     
+    void printState(uint64 X){
+        for (int i = 0 ; i < gameConfig->BoardSize ; i++){
+            for (int j = 0 ; j < gameConfig->BoardSize; j++){
+                int r = i*gameConfig->BoardSize + j;
+                if (X & ( 1ULL << r))
+                    cout << "1 ";
+                else
+                    cout << "0 ";
+                
+            }
+            cout << endl;
+        }
+    }
     
+    Move makeMove(string move,int colour){
+        Move to_ret;
+        switch(move[0]){
+            case 'F': to_ret.piece.type = FlatStone;
+                break;
+            case 'S': to_ret.piece.type = StandingStone;
+                break;
+            case 'C': to_ret.piece.type = Capstone;
+                break;
+            }
+        to_ret.piece.color = (Color) colour;
+        to_ret.column = move[1]-'a';
+        to_ret.row = move[2]-'1';
+        return to_ret;
+    }
     
+    void slideMove(Move move){
+        int r = move.row;
+        int c = move.column;
+        int i = r * gameConfig->BoardSize + c;
+        uint64 pickupStack = Stacks[i];
+        
+        int numPieces = 0;
+        for (int j=0; j<move.dropLength; j++){
+            numPieces += move.Drops[j];
+        }
+        
+        if (numPieces == Heights[i]){
+            WhitePieces &= ~(1ULL << i);
+            BlackPieces &= ~(1ULL << i);
+        } else {
+            if ((pickupStack & (1ULL << numPieces)) != 0){
+                WhitePieces |= (1ULL << i);
+                BlackPieces &= ~(1ULL << i);
+            } else {
+                BlackPieces |= (1ULL << i);
+                WhitePieces &= ~(1ULL << i);
+            }
+        }
+        
+        Stacks[i] >>= numPieces;
+        Heights[i] -= numPieces;
+        
+        
+        int dr = 0;
+        int dc = 0;
+        switch(move.Movetype){
+            case SlideUp: dr = 1;
+                break;
+            case SlideDown: dr = -1;
+                break;
+            case SlideLeft: dc = -1;
+                break;
+            case SlideRight: dc = 1;
+                break;
+        }
+        
+        int j = 0;
+        int piecesToDropHere = 0;
+        while (numPieces>0){
+            r += dr;
+            c += dc;
+            i = r * gameConfig->BoardSize + c;
+            piecesToDropHere = move.Drops[j++];
+            
+            Standing &= ~(1ULL << i);
+            
+            uint64 drop = (pickupStack >> (numPieces - piecesToDropHere)) & ((1ULL << piecesToDropHere) - 1);
+            Stacks[i] = (Stacks[i] << piecesToDropHere) | drop;
+            Heights[i] += piecesToDropHere;
+            
+            if ((Stacks[i] & 1) != 0){
+                WhitePieces |= (1ULL << i);
+                BlackPieces &= ~(1ULL << i);
+            } else {
+                WhitePieces &= ~(1ULL << i);
+                BlackPieces |= (1ULL << i);
+            }
+            
+            numPieces -= piecesToDropHere;
+        }
+        
+        int i_or = move.row * gameConfig->BoardSize + move.column;
+        
+        if ((CapStones & (1ULL << i_or)) != 0){
+            CapStones |= (1ULL << i);
+            CapStones &= ~(1ULL << i_or);
+        }
+        else if ((Standing & (1ULL << i_or)) != 0){
+            Standing |= (1ULL << i);
+            Standing &= ~(1ULL << i_or);
+        }
+    }
+
 };
--- aBeautifulPlayer/main.cpp
+++ aBeautifulPlayer/main.cpp
@@ -7,10 +7,47 @@
 //
 
 #include <stdio.h>
-#include <iostream>
-
+#include "Game.cpp"
+#include <bitset>
 using namespace std;
 
+void printGameState(Game g){
+    for (int r=g.gameConfig->BoardSize-1; r>-1; r--){
+        for (int c=0; c<g.gameConfig->BoardSize; c++){
+            cerr << "[";
+            for (int k =g.Heights[g.gameConfig->BoardSize*r + c]-1; k>-1; k--)
+                if ((g.Stacks[g.gameConfig->BoardSize*r + c] & (1<<k)) != 0)
+                    cerr << "W";
+                else
+                    cerr << "B";
+            cerr << "]\t";
+        }
+        cerr << endl;
+    }
+}
+
+
 int main(){
+    Game myGame(5);
+    
+    myGame.PlaceMove(myGame.makeMove("Fa1",1));
+    myGame.PlaceMove(myGame.makeMove("Fb1",0));
+    myGame.PlaceMove(myGame.makeMove("Wc1",1));
+    myGame.PlaceMove(myGame.makeMove("Fd1",0));
+    myGame.PlaceMove(myGame.makeMove("Fa2",1));
+    myGame.PlaceMove(myGame.makeMove("Ce5",0));
+
+    myGame.printState(myGame.WhitePieces);
+    cout << endl;
+    myGame.printState(myGame.BlackPieces);
+    cout << endl;
+    myGame.printState(myGame.CapStones);
+    cout << endl;
+    myGame.printState(myGame.Standing);
+    cout << endl;
+    
+    myGame.FindComponents();
+    cout << bitset<64>(myGame.WhiteComponents[0]) << endl;
+
     cout << sizeof(unsigned long long) << endl;
 }
